af556eca3f371898c12e9302a7e127a765eb8330
SangHun Lee <huny22@etribe.co.kr>
2023-01-05 12:31:24 +0900

    20230105 수정

--- src/main/java/com/shop/constant/ItemSellStatus.java
+++ src/main/java/com/shop/constant/ItemSellStatus.java
@@ -1,6 +1,6 @@
 package com.shop.constant;
 
 public enum ItemSellStatus {
-    SELL,
-    SOLD_OUT
+  SELL,
+  SOLD_OUT
 }
--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -13,27 +13,27 @@ import java.time.LocalDateTime;
 @Table(name = "")
 public class Item {
 
-    @Id
-    @Column(name = "item_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+  @Id
+  @Column(name = "item_id")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    @Column(nullable = false, length = 50)
-    private String itemNm;
+  @Column(nullable = false, length = 50)
+  private String itemNm;
 
-    @Column(name = "price", nullable = false)
-    private int price;
+  @Column(name = "price", nullable = false)
+  private int price;
 
-    @Column(nullable = false)
-    private int stockNumber;
+  @Column(nullable = false)
+  private int stockNumber;
 
-    @Lob
-    @Column(nullable = false)
-    private String itemDetail;
+  @Lob
+  @Column(nullable = false)
+  private String itemDetail;
 
-    @Enumerated(EnumType.STRING)
-    private ItemSellStatus itemSellStatus;
+  @Enumerated(EnumType.STRING)
+  private ItemSellStatus itemSellStatus;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 }
