184ff06f1ca86b112a3bf3973fa1e415185d1b2d
SangHun Lee <huny22@etribe.co.kr>
2023-01-09 16:57:01 +0900

    20230109 커밋

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -3,16 +3,37 @@ package com.shop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return null;
+    http.formLogin()
+        .loginPage("/members/login")  // 로그인 페이지 URL 설정
+        .defaultSuccessUrl("/")       // 로그인 성공 시 이동할 URL
+        .usernameParameter("email")   // 로그인 시 사용할 파라미터 이름으로 email을 지정
+        .failureUrl("/members/login/error")   // 로그인 실패 시 이동할 URL을 설정
+        .and()
+        .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) // 로그아웃 URL 설정
+        .logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 URL
+    ;
+
+    http.authorizeRequests()
+        .mvcMatchers("/css/**", "/js/**", "/img/**").permitAll()
+        .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+        .mvcMatchers("/admin/**").hasRole("ADMIN")
+        .anyRequest().authenticated()
+    ;
+
+    return http.build();
   }
 
   @Bean
--- src/test/java/com/shop/service/MemberServiceTest.java
+++ src/test/java/com/shop/service/MemberServiceTest.java
@@ -43,4 +43,17 @@ class MemberServiceTest {
     assertEquals(member.getPassword(), savedMember.getPassword());
     assertEquals(member.getRole(), savedMember.getRole());
   }
+
+  @Test
+  @DisplayName("중복 회원 가입 테스트")
+  public void saveDuplicateMemberTest() {
+    Member member1 = new Member();
+    Member member2 = new Member();
+    memberService.saveMember(member1);
+
+    Throwable e = assertThrows(IllegalStateException.class, () -> {
+      memberService.saveMember(member2);
+    });
+    assertEquals("이미 가입된 회원입니다.", e.getMessage());
+  }
 }
