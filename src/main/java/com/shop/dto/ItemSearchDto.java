1c03d749237e3d97bd8b1f07c3354570dbf06349
SangHun Lee <huny22@etribe.co.kr>
2023-01-11 16:28:56 +0900

    20230111 커밋

--- src/main/java/com/shop/dto/ItemSearchDto.java
+++ src/main/java/com/shop/dto/ItemSearchDto.java
@@ -1,4 +1,15 @@
 package com.shop.dto;
 
+import com.shop.constant.ItemSellStatus;
+import lombok.Data;
+
+@Data
 public class ItemSearchDto {
+  private String searchDateType;
+
+  private ItemSellStatus searchSellStatus;
+
+  private String searchBy;
+
+  private String searchQuery = "";
 }
